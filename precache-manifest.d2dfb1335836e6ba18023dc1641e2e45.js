50bd850edab36749fd05b93551a9209258ab1c8b
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-04 11:40:59 +0600

    Updates

---
+++ precache-manifest.d2dfb1335836e6ba18023dc1641e2e45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45218b59845090ff4aacd0fe64196838",
+    "url": "/sign-in-page/index.html"
+  },
+  {
+    "revision": "dc1350560fe30f6ccdac",
+    "url": "/sign-in-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "94c4e66850461b55923b",
+    "url": "/sign-in-page/static/js/2.56886c46.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/sign-in-page/static/js/2.56886c46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc1350560fe30f6ccdac",
+    "url": "/sign-in-page/static/js/main.72947bd0.chunk.js"
+  },
+  {
+    "revision": "c316b8f9bee104cbff4f",
+    "url": "/sign-in-page/static/js/runtime-main.0c2c8b05.js"
+  }
+]);
